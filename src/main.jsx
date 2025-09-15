87ab45e78d0738e13276c1167dc3c3f4832dd243
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-09-15 11:51:26 +0600

    component + route added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./components/Home";
+import AddCoffee from "./components/AddCoffee";
+import UpdateCoffee from "./components/UpdateCoffee";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "addCoffee",
+        Component: AddCoffee,
+      },
+      {
+        path: "updateCoffee",
+        Component: UpdateCoffee,
+      },
+    ],
   },
 ]);
 
